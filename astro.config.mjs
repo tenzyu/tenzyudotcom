98461755d4a6f7e53207f12ad46d83a02fb5eb90
tenzyu <tenzyu.on@gmail.com>
2024-09-25 22:37:45 +0000

    fix: domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://tenzyu.com',
 	integrations: [mdx(), sitemap()],
 	server: {
  		host: '127.0.0.1'
