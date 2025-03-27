6ebe5ebce11be1d6ab2326073801ea3094e28110
tenzyu <tenzyu.on@gmail.com>
2025-03-27 19:29:05 +0900

    chore: upgrade astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import tailwindcss from '@tailwindcss/vite';
 
+import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://tenzyu.com',
-	integrations: [mdx(), sitemap()],
-	server: {
- 		host: '127.0.0.1'
-	}
+  site: 'https://tenzyu.com',
+  integrations: [mdx(), sitemap()],
+
+  server: {
+      host: '127.0.0.1'
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
