ef05bb3fd2d26eb56dc097407a9a6b37f34e143e
tenzyu <tenzyu.on@gmail.com>
2025-04-06 23:47:41 +0900

    cosmetic: linktree

--- src/components/link-card.tsx
+++ src/components/link-card.tsx
@@ -1,24 +1,58 @@
-import { Button } from '@/components/ui/button'
+'use client'
+
 import type { MyLink } from '@/data/links'
-import { ShareDialog } from './share-dialog'
+import Image from 'next/image'
+import Link from 'next/link'
 
 interface LinkCardProps {
   link: MyLink
 }
 
 export function LinkCard({ link }: LinkCardProps) {
+  const getIcon = (name: string) => {
+    const filename = (() => {
+      switch (name.toLowerCase()) {
+        case 'github':
+          return 'github'
+        case 'twitter':
+          return 'x'
+        case 'twitch':
+          return 'twitch'
+        case 'youtube':
+          return 'youtube'
+        case 'notion':
+          return 'notion'
+        case 'discord':
+          return 'discord'
+        case 'osu!':
+          return 'osu'
+        case 'reddit':
+        case 'reddit (old)':
+          return 'reddit'
+      }
+    })()
+
+    return (
+      <Image src={`/icons/${filename}.svg`} width={44} height={44} alt={''} />
+    )
+  }
+
   return (
-    <div className='flex items-center justify-between border border-md rounded-md'>
-      <Button
-        variant='ghost'
-        className='flex-1 justify-start px-4 py-4 h-auto text-lg hover:bg-muted/50'
-        asChild={true}
-      >
-        <a href={`/u/${link.shortenUrl}`} target='_blank' rel='noreferrer'>
+    <Link
+      href={`/u/${link.shortenUrl}`}
+      className='block'
+      target='_blank'
+      rel='noreferrer'
+    >
+      <div className='border rounded-lg p-4 flex flex-col items-center text-center hover:bg-gray-900 transition-colors'>
+        <div className='rounded-full p-2 mb-3 dark:bg-[#ccc]'>
+          {getIcon(link.name)}
+        </div>
+        <h3 className='text-lg font-medium dark:text-white mb-1'>
           {link.name}
-        </a>
-      </Button>
-      <ShareDialog title={link.name} url={link.shortenUrl} />
-    </div>
+        </h3>
+        <p className='text-gray-400 text-sm'>{link.id}</p>
+      </div>
+    </Link>
   )
 }
--- src/components/link-list.tsx
+++ src/components/link-list.tsx
@@ -1,17 +1,14 @@
-import { Card, CardContent } from '@/components/ui/card'
 import { MY_LINKS } from '@/data/links'
 import { LinkCard } from './link-card'
 
 export function LinkList() {
   return (
-    <Card className='w-full max-w-md mx-auto'>
-      <CardContent className='px-6'>
-        <div className='space-y-2'>
-          {MY_LINKS.map(link => (
-            <LinkCard key={link.shortenUrl} link={link} />
-          ))}
-        </div>
-      </CardContent>
-    </Card>
+    <div className='w-full max-w-4xl mx-auto'>
+      <div className='grid grid-cols-2 sm:grid-cols-3 gap-4'>
+        {MY_LINKS.map(link => (
+          <LinkCard key={link.shortenUrl} link={link} />
+        ))}
+      </div>
+    </div>
   )
 }
