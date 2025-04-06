60a48ed10ae729116509bad69c228d2de7728b36
tenzyu <tenzyu.on@gmail.com>
2025-04-06 09:16:19 +0900

    fix: light theme of keyboard visualization

--- src/components/osu-settings.tsx
+++ src/components/osu-settings.tsx
@@ -75,13 +75,13 @@ export function OsuSettings() {
                     <CardTitle className="pb-4 text-lg">Keyboard</CardTitle>
 
                     {/* SayoDevice O3C keyboard visualization */}
-                    <div className="relative bg-[#2d3642] dark:bg-[#2d3642] rounded-md p-4 max-w-[400px] mx-auto aspect-[4/3]">
+                    <div className="relative bg-gray-100 dark:bg-[#2d3642] rounded-md p-4 max-w-[400px] mx-auto aspect-[4/3]">
 
                         {/* Knob */}
-                        <div className="absolute top-[4%] left-[3%] w-34 h-34 rounded-full bg-[#252c38] dark:bg-[#252c38] flex items-center justify-center">
-                            <div className="w-20 h-20 rounded-full bg-[#1e2530] dark:bg-[#1e2530] relative flex items-center justify-center">
+                        <div className="absolute top-[4%] left-[3%] w-34 h-34 rounded-full bg-gray-200 dark:bg-[#252c38] flex items-center justify-center">
+                            <div className="w-20 h-20 rounded-full bg-gray-300 dark:bg-[#1e2530] relative flex items-center justify-center">
                                 {/* Knob center */}
-                                <div className="text-gray-300 text-lg font-medium">Y</div>
+                                <div className="dark:text-gray-300 text-lg font-medium">Y</div>
 
                                 {/* Rotation indicators */}
                                 <div className="absolute -left-5 top-1/2 transform -translate-y-1/2 text-gray-400">
@@ -95,14 +95,14 @@ export function OsuSettings() {
 
                         {/* Keys */}
                         <div className="absolute bottom-[5%] left-0 right-0 flex justify-center gap-2">
-                            <div className="w-29 h-29 bg-[#252c38] dark:bg-[#252c38] rounded-md flex items-center justify-center">
-                                <span className="text-lg font-medium text-gray-300">C</span>
+                            <div className="w-29 h-29 bg-gray-300 dark:bg-[#252c38] rounded-md flex items-center justify-center">
+                                <span className="text-lg font-medium dark:text-gray-300">C</span>
                             </div>
-                            <div className="w-29 h-29 bg-[#252c38] dark:bg-[#252c38] rounded-md flex items-center justify-center">
-                                <span className="text-lg font-medium text-gray-300">Z</span>
+                            <div className="w-29 h-29 bg-gray-300 dark:bg-[#252c38] rounded-md flex items-center justify-center">
+                                <span className="text-lg font-medium dark:text-gray-300">Z</span>
                             </div>
-                            <div className="w-29 h-29 bg-[#252c38] dark:bg-[#252c38] rounded-md flex items-center justify-center">
-                                <span className="text-lg font-medium text-gray-300">X</span>
+                            <div className="w-29 h-29 bg-gray-300 dark:bg-[#252c38] rounded-md flex items-center justify-center">
+                                <span className="text-lg font-medium dark:text-gray-300">X</span>
                             </div>
                         </div>
                     </div>
