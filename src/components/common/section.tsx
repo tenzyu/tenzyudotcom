de49bb6585681c0e18ac6ba80091132a989c4dd3
tenzyu <tenzyu.on@gmail.com>
2025-05-03 10:22:58 +0900

    feat: implement Table of Contents component and refactor sections in Home page for better organization and navigation

--- src/components/common/section.tsx
+++ src/components/common/section.tsx
@@ -2,11 +2,16 @@ import { cn } from '@/lib/utils'
 
 type SectionProps = {
   children: React.ReactNode
+  id?: string
 } & React.HTMLAttributes<HTMLElement>
 
-export function Section({ children, className, ...props }: SectionProps) {
+export function Section({ children, className, id, ...props }: SectionProps) {
   return (
-    <section className={cn('py-6 sm:py-8 md:py-8', className)} {...props}>
+    <section
+      id={id}
+      className={cn('py-6 sm:py-8 md:py-8', className)}
+      {...props}
+    >
       {children}
     </section>
   )
