9ffcf8c5d535158b24a37221e848148eacc95610
tenzyu <tenzyu.on@gmail.com>
2026-03-13 00:41:12 +0900

    feat: add 'I Am a Stray LLM Harness Engineer' blog post and register `KoFiLink` and `OtakuAside` MDX components.

--- src/app/[locale]/(main)/blog/_features/mdx-components.tsx
+++ src/app/[locale]/(main)/blog/_features/mdx-components.tsx
@@ -1,5 +1,9 @@
+import { KoFiLink } from '@/app/[locale]/_features/shell/kofi-link'
+import { OtakuAside } from '../../_features/otaku-aside'
 import { CautionAlert } from './caution-alert'
 
 export const components = {
+  OtakuAside,
   CautionAlert,
+  KoFiLink
 }
