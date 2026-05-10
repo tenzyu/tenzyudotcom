bf6dbda430e45020374af5ec3f4497ee9ab4acd8
tenzyu <tenzyu.on@gmail.com>
2026-05-11 07:42:17 +0900

    chore(osu-skin-node): remove no need package

--- product/packages/osu-skin-node/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./lib/server/archive";
-export * from "./lib/server/fs-path";
-export * from "./lib/server/project-service";
-export * from "./lib/server/project-store";
-export * from "./lib/server/skin-context";
