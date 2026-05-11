e6cfeaee1e95d95fa4e2967c92e582c87038e842
tenzyu <tenzyu.on@gmail.com>
2026-05-11 10:35:05 +0900

    fix: pinned nixos-unstable for nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "tenzyu.com monorepo";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     serena.url = "github:oraios/serena";
   };
